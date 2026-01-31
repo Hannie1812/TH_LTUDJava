70ae8584a2026f151d5633b015f7d4e7cc835655
Hannie1812 <bichhang18122004@gmail.com>
2026-01-31 16:03:00 +0700

    Bài 5_Xây dựng chức năng Checkout

---
+++ src/main/java/com/nbhang/repositories/IItemInvoiceRepository.java
@@ -0,0 +1,8 @@
+package com.nbhang.repositories;
+
+import com.nbhang.entities.ItemInvoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface IItemInvoiceRepository extends JpaRepository<ItemInvoice, Long>{
+}
--- src/main/java/com/nbhang/services/CartService.java
+++ src/main/java/com/nbhang/services/CartService.java
@@ -2,14 +2,28 @@ package com.nbhang.services;
 
 import com.nbhang.daos.Cart;
 import com.nbhang.daos.Item;
+import com.nbhang.entities.Invoice;
+import com.nbhang.entities.ItemInvoice;
+import com.nbhang.repositories.IBookRepository;
+import com.nbhang.repositories.IInvoiceRepository;
+import com.nbhang.repositories.IItemInvoiceRepository;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(isolation = Isolation.SERIALIZABLE, rollbackFor = { Exception.class, Throwable.class })
 public class CartService {
     private static final String CART_SESSION_KEY = "cart";
+    private final IInvoiceRepository invoiceRepository;
+    private final IItemInvoiceRepository itemInvoiceRepository;
+    private final IBookRepository bookRepository;
 
     public Cart getCart(@NotNull HttpSession session) {
         return Optional.ofNullable((Cart) session.getAttribute(CART_SESSION_KEY))
@@ -39,5 +53,24 @@ public class CartService {
                 .mapToDouble(item -> item.getPrice() *
                         item.getQuantity())
                 .sum();
+    }
+
+    public void saveCart(@NotNull HttpSession session) {
+        var cart = getCart(session);
+        if (cart.getCartItems().isEmpty())
+            return;
+        var invoice = new Invoice();
+        invoice.setInvoiceDate(new Date(new Date().getTime()));
+        invoice.setPrice(getSumPrice(session));
+        invoiceRepository.save(invoice);
+        cart.getCartItems().forEach(item -> {
+            var items = new ItemInvoice();
+            items.setInvoice(invoice);
+            items.setQuantity(item.getQuantity());
+            items.setBook(bookRepository.findById(item.getBookId())
+                    .orElseThrow());
+            itemInvoiceRepository.save(items);
+        });
+        removeCart(session);
     }
 }
